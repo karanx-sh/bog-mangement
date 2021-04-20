a1d84adb458d98dfc426f39abf3cace6b7d480c1
kphacko <25796369+kphacko@users.noreply.github.com>
2021-04-20 23:38:06 +0530

    changes added

--- connection.js
+++ connection.js
@@ -1,23 +1,3 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASS,
-
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port:process.env.DB_PORT,
-
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000,
-    },
-
-    define: {
-      timestamps: false,
-    },
-  }
-);
+module.exports = new Sequelize(process.env.DATABASE_URL);

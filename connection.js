7121ac7b533b01b7158ac5d36e5169bcd99bf61d
kphacko <25796369+kphacko@users.noreply.github.com>
2021-04-20 21:32:22 +0530

    postgre sql added

--- connection.js
+++ connection.js
@@ -4,9 +4,11 @@ module.exports = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
   process.env.DB_PASS,
+
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: "postgres",
+    port:process.env.DB_PORT,
 
     pool: {
       max: 5,
